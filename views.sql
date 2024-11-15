dcb73784f31e6a32b4402fabf90626451787a096
catarinaUni <catarina.sampaio@discente.univasf.edu.br>
2024-11-15 15:43:26 -0300

    Update views.sql

--- views.sql
+++ views.sql
@@ -10,15 +10,6 @@ FROM
 
 
 
-SELECT * FROM visao_status_tarefas LIMIT 10;
-
-
-
-
-
-
-
-
 CREATE VIEW visao_comparativo_status_tarefas AS
 SELECT 
     rp.projeto_id,
@@ -36,4 +27,42 @@ GROUP BY
 
 
 
-SELECT * FROM visao_comparativo_status_tarefas LIMIT 10;
+
+
+
+-- views adicionais
+
+
+--visualizar projetos que estão em andamento no momento
+
+CREATE VIEW vw_projetos_em_andamento AS
+SELECT p.id AS projeto_id,
+       p.nome AS projeto_nome,
+       p.status AS projeto_status,
+       p.data_inicio,
+       p.data_fim_prevista,
+       c.nome_usuario AS cliente_nome
+FROM projetos p
+JOIN clientes c ON p.cliente_id = c.id
+WHERE p.status != 'concluído';
+
+
+-- quais materiais no estoque estão abaixo der 1000 unidades e seus respectivos fornecedores para possíveis novos pedidos
+
+CREATE VIEW vw_materiais_estoque_abaixo_1000 AS
+SELECT 
+    m.id AS material_id,
+    m.nome AS material_nome,
+    im.quantidade AS quantidade_estoque,
+    f.id AS fornecedor_id,
+    f.nome AS fornecedor_nome
+FROM 
+    materiais m
+JOIN 
+    inventario_materiais im ON m.id = im.material_id
+JOIN 
+    pedidos_materiais pm ON m.id = pm.material_id
+JOIN 
+    fornecedores f ON pm.fornecedor_id = f.id
+WHERE 
+    im.quantidade < 1000;
