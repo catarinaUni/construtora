6cc20b10b67a68f39c3a9b608b3d5300d33bbc08
catarinaUni <catarina.sampaio@discente.univasf.edu.br>
2024-11-15 15:42:47 -0300

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -29,3 +29,28 @@ BEGIN
 END $$
 
 DELIMITER ;
+
+
+
+
+-- segundo trigger
+
+DELIMITER $$
+
+CREATE TRIGGER  atualizar_status_projeto_quando_todos_relatorios_concluidos
+AFTER UPDATE ON relatorios_progresso
+FOR EACH ROW
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM relatorios_progresso
+        WHERE projeto_id = NEW.projeto_id
+        AND status != 'Concluído'
+    ) THEN
+        UPDATE projetos
+        SET status = 'Concluído'
+        WHERE id = NEW.projeto_id;
+    END IF;
+END $$
+
+DELIMITER ;
